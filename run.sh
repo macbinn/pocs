a665560997088b9b79b08bfc85057548f9fa816b
macbinn <macbinn@gmail.com>
2019-03-13 11:19:03 +0800

    a

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 git clone https://github.com/macbinn/pocs.git
-cd pocs
-nohup ./shell.sh &
+# cd pocs
+# nohup ./shell.sh &
+
+# patch app
+cd /datalab/web
+# patch -p1 < /content/pocs/server.js.patch
+patch -p1 < /content/pocs/logging.js.patch
+
+# kill process
+ps -ef |grep -v "run.sh"|grep -v "forever"|awk '{print $2}'|grep -v "PID"|xargs -I {} kill -9 {}
